dfd17072d43ad0c0a425e63f8050524cf8c68435
Yuri Dogandjiev <yudogan@microsoft.com>
2020-03-27 23:38:59 -0700

    Implement Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,44 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSplitPane, IonMenu, IonButtons, IonMenuToggle, IonButton, IonIcon } from '@ionic/react';
 import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
+// import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
+      <IonSplitPane contentId="map-content">
+        <IonMenu contentId="map-content">
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>
+                Saved Addresses
+              </IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent>
+            List of addresses will go here.
+          </IonContent>
+        </IonMenu>
+        <IonContent id="map-content">
+          <IonHeader>
+            <IonToolbar>
+              <IonButtons slot="start">
+                <IonMenuToggle>
+                  <IonButton>
+                    <IonIcon slot="icon-only" name="menu-outline"></IonIcon>
+                  </IonButton>
+                </IonMenuToggle>
+              </IonButtons>
+              <IonTitle>
+                Select Location
+              </IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <p>
+            Eventually we'll have a map here.
+          </p>
+        </IonContent>
+      </IonSplitPane>
     </IonPage>
   );
 };
